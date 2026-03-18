1dbf9d2edcc49cbc94e3af7104865f3ddb9059d7
jokeoa <itzjokeoa@gmail.com>
2026-03-18 12:20:00 +0500

    feat(service): improve CreateShipment method

--- internal/usecase/shipment.go
+++ internal/usecase/shipment.go
@@ -1,17 +1,29 @@
 package usecase
 
-import "tracker-task/internal/domain"
+import (
+	"fmt"
+	"tracker-task/internal/domain"
+)
 
 type ShipmentService struct {
 	shipmentRepo domain.ShipmentRepository
 	eventRepo    domain.EventRepository
 }
 
-func (s *ShipmentService) CreateShipment(ref, origin, dest string, unit []domain.Unit) (*domain.Shipment, error) {
-	if ref == "" || origin == "" || dest == "" || len(unit) == 0 {
-		return nil, domain.ErrInvalidShipmentData
+func (s *ShipmentService) CreateShipment(ref, origin, dest string, units []domain.Unit) (*domain.Shipment, error) {
+	if ref == "" || origin == "" || dest == "" || len(units) == 0 {
+		return nil, fmt.Errorf("reference number, origin, destination and units are required")
 	}
-	shipment := domain.NewShipment(ref, origin, dest)
-	return shipment, nil
 
+	shipment := domain.NewShipment(ref, origin, dest)
+
+	for _, u := range units {
+		shipment.AddUnit(u)
+	}
+
+	if err := s.shipmentRepo.SaveShipment(shipment); err != nil {
+		return nil, err
+	}
+
+	return shipment, nil
 }
