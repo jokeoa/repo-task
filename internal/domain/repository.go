783a05f083d8d557250553dd3f0b20806bbf3b94
jokeoa <itzjokeoa@gmail.com>
2026-03-18 09:40:00 +0500

    feat(repository): add methods to control event model

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -1,6 +1,13 @@
 package domain
 
+import "github.com/google/uuid"
+
 type ShipmentRepository interface {
 	SaveShipment(shipment Shipment) error
-	GetShipmentByID(id string) (Shipment, error)
+	GetShipmentByID(id uuid.UUID) (Shipment, error)
+}
+
+type EventRepository interface {
+	SaveEvent(event ShipmentEvent) error
+	GetEventsByShipmentID(shipmentID uuid.UUID) ([]ShipmentEvent, error)
 }
