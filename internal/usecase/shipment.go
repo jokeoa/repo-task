33f219a3f6127e1aa4917bd5c7a3d19ae2eb1c8d
jokeoa <itzjokeoa@gmail.com>
2026-03-18 11:00:00 +0500

    feat(service): add NewShipment method

--- internal/usecase/shipment.go
+++ internal/usecase/shipment.go
@@ -1,1 +1,17 @@
 package usecase
+
+import "tracker-task/internal/domain"
+
+type ShipmentService struct {
+	shipmentRepo domain.ShipmentRepository
+	eventRepo    domain.EventRepository
+}
+
+func (s *ShipmentService) CreateShipment(ref, origin, dest string, unit []domain.Unit) (*domain.Shipment, error) {
+	if ref == "" || origin == "" || dest == "" || len(unit) == 0 {
+		return nil, domain.ErrInvalidShipmentData
+	}
+	shipment := domain.NewShipment(ref, origin, dest)
+	return shipment, nil
+
+}
